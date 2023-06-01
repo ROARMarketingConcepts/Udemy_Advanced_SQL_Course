1bc030b57dda2a66947c46ee0aedd50410e8b06f
Ken Wood <ken@roarmarketingconcepts.com>
2023-06-01 08:15:41 -0400

    Add files via upload

--- Assignments/Final_Project.sql
+++ Assignments/Final_Project.sql
@@ -43,7 +43,62 @@ FROM website_sessions ws
 LEFT JOIN orders o
 ON ws.website_session_id=o.website_session_id
 WHERE DATE(ws.created_at) < '2015-03-15'
-GROUP BY 1,2
+GROUP BY 1,2;
 
 -- 4.  Next, let’s show the overall session to order conversion rate trends for those same channels, by quarter.
 -- 		Please also make a note of any periods where we made major improvements or optimizations.
+
+SELECT 
+	YEAR(ws.created_at) AS year,
+	QUARTER(ws.created_at) AS quarter,
+	COUNT(DISTINCT CASE WHEN utm_source IS NULL AND utm_campaign IS NULL AND http_referer IS NULL THEN order_id END) / 
+	COUNT(DISTINCT CASE WHEN utm_source IS NULL AND utm_campaign IS NULL AND http_referer IS NULL THEN ws.website_session_id END) AS direct_type_in,
+    COUNT(DISTINCT CASE WHEN utm_source IS NULL AND utm_campaign IS NULL AND http_referer IS NOT NULL THEN order_id END) /
+    COUNT(DISTINCT CASE WHEN utm_source IS NULL AND utm_campaign IS NULL AND http_referer IS NOT NULL THEN ws.website_session_id END) AS organic_search,
+	COUNT(DISTINCT CASE WHEN utm_campaign='brand' THEN order_id END) / 
+    COUNT(DISTINCT CASE WHEN utm_campaign='brand' THEN ws.website_session_id END) AS brand_search_overall,
+	COUNT(DISTINCT CASE WHEN utm_source='gsearch' AND utm_campaign='nonbrand' THEN order_id END) /
+    COUNT(DISTINCT CASE WHEN utm_source='gsearch' AND utm_campaign='nonbrand' THEN ws.website_session_id END) AS gsearch_nonbrand,
+	COUNT(DISTINCT CASE WHEN utm_source='bsearch' AND utm_campaign='nonbrand' THEN order_id END) /
+    COUNT(DISTINCT CASE WHEN utm_source='bsearch' AND utm_campaign='nonbrand' THEN ws.website_session_id END) AS bsearch_nonbrand
+	-- COUNT(DISTINCT CASE WHEN utm_source='socialbook' THEN order_id END) AS social
+FROM website_sessions ws
+LEFT JOIN orders o
+ON ws.website_session_id=o.website_session_id
+WHERE DATE(ws.created_at) < '2015-03-15'
+GROUP BY 1,2;
+
+-- 5. 	We’ve come a long way since the days of selling a single product. Let’s pull monthly trending for revenue 
+-- 		and margin by product, along with total sales and revenue. Note anything you notice about seasonality.
+
+SELECT 
+	
+    YEAR(created_at) AS year,
+    MONTH(created_at) AS month,
+    COUNT(CASE WHEN product_id=1 THEN product_id ELSE NULL END) AS mrfuzzy_quantity_sold,
+	ROUND(SUM(CASE WHEN product_id=1 THEN price_usd ELSE 0 END),0) AS mrfuzzy_revenue,
+    ROUND(SUM(CASE WHEN product_id=1 THEN price_usd-cogs_usd ELSE 0 END),0) AS mrfuzzy_margin,
+    COUNT(CASE WHEN product_id=2 THEN product_id ELSE NULL END) AS lovebear_quantity_sold,
+	ROUND(SUM(CASE WHEN product_id=2 THEN price_usd ELSE 0 END),0) AS lovebear_revenue,
+    ROUND(SUM(CASE WHEN product_id=2 THEN price_usd-cogs_usd ELSE 0 END),0) AS lovebear_margin,
+    COUNT(CASE WHEN product_id=3 THEN product_id ELSE NULL END) AS birthdaybear_quantity_sold,
+	ROUND(SUM(CASE WHEN product_id=3 THEN price_usd ELSE 0 END),0) AS birthdaybear_revenue,
+    ROUND(SUM(CASE WHEN product_id=3 THEN price_usd-cogs_usd ELSE 0 END),0) AS birthdaybear_margin,
+    COUNT(CASE WHEN product_id=4 THEN product_id ELSE NULL END) AS minibear_quantity_sold,
+	ROUND(SUM(CASE WHEN product_id=4 THEN price_usd ELSE 0 END),0) AS minibear_revenue,
+    ROUND(SUM(CASE WHEN product_id=4 THEN price_usd-cogs_usd ELSE 0 END),0) AS minibear_margin
+FROM order_items
+GROUP BY 1,2
+
+
+
+-- 6.	Let’s dive deeper into the impact of introducing new products. Please pull monthly sessions to the /products 
+-- 		page, and show how the % of those sessions clicking through another page has changed over time, along with
+-- 		a view of how conversion from /products to placing an order has improved.
+
+-- 7.  We made our 4th product available as a primary product on December 05, 2014 (it was previously only a cross sell
+-- 		item). Could you please pull sales data since then, and show how well each product cross sells from one another?
+
+-- 8. 	In addition to telling investors about what we’ve already achieved, let’s show them that we still have plenty of
+-- 		gas in the tank. Based on all the analysis you’ve done, could you share some recommendations and
+-- 		opportunities for us going forward? No right or wrong answer here I’d just like to hear your perspective!
